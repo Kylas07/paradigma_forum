d72cc09c51b78f621d7aa67d6dc170c6437ee05c
Kylas07 <108191192+Kylas07@users.noreply.github.com>
2022-12-04 15:23:37 +0100

    Update zonemonstre.js

--- zonemonstre.js
+++ zonemonstre.js
@@ -1,3 +1,4 @@
+console.log ("scriptcharge")
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
@@ -5,13 +6,14 @@ function filterSelection(c) {
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    w3RemoveClass(x[i], "balise_show");
+    if (x[i].className.indexOf(c) > -1) { w3AddClass(x[i], "balise_show"); }
   }
 }
 
 // Show filtered elements
 function w3AddClass(element, name) {
+  console.log("addclass "+name)
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -24,6 +26,7 @@ function w3AddClass(element, name) {
 
 // Hide elements that are not selected
 function w3RemoveClass(element, name) {
+  console.log("removeclass "+name)
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
